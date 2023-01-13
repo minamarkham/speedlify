2dfa3efb13aed550c0a29b951cf24565a8197636
Mina Markham <mmarkham@slack-corp.com>
2023-01-13 09:43:27 -0800

    update sites

---
+++ _data/sites/bey.js
@@ -0,0 +1,12 @@
+module.exports = {
+	description: "Beyoncé Projects",
+	options: {
+		frequency: 60 * 11 + 30, // 11h, 30m
+		freshChrome: "run",
+	},
+	urls: [
+		"http://yoncetheme.com",
+		"https://slay.sh",
+		"https://httbey.com"
+	]
+};
---
+++ _data/sites/cupcake.js
@@ -0,0 +1,21 @@
+module.exports = {
+	name: 'mina.codes',
+	description: 'Personal website',
+	options: {
+		frequency: 60 * 23, // 23 hours
+		freshChrome: "site"
+	},
+	options: {
+		frequency: 1,
+		freshChrome: 'site'
+	},
+	urls: [
+		'https://mina.codes/',
+		'https://mina.codes/posts/',
+		'https://mina.codes/projects/',
+		'https://mina.codes/speaking/',
+		'https://mina.codes/speaker-kit/',
+		'https://mina.codes/about/'
+	],
+	skip: false
+}
